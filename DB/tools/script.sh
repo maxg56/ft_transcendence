53aef35250268876c70c05b366b22440a688ddb5
maxg56 <mgendrot56@gmail.com>
2025-03-21 16:51:38 +0100

    add | DB conf

--- DB/tools/script.sh
+++ DB/tools/script.sh
@@ -2,17 +2,21 @@
 
 set -e
 
-/usr/bin/mariadbd-safe --datadir=/var/lib/mysql &
+# Démarrer MariaDB
+mariadbd --datadir=/var/lib/mysql &
 
 echo "Waiting for MariaDB to be ready..."
 until mariadb-admin ping --silent; do
-	sleep 2
+    sleep 2
 done
-echo "MariaDB is ready"
-# Run the SQL script
+echo "✅ MariaDB is ready"
+
+# Vérifier si le fichier init.sql existe et l'exécuter
 if [ -f "/tools/init.sql" ]; then
-	envsubst < /tools/init.sql | mariadb
-	
+    envsubst < /tools/init.sql | mariadb
 fi
-echo "MariaDB is ready"
+
+echo "✅ Initialisation SQL terminée"
+
+# Empêcher le conteneur de se fermer immédiatement
 wait

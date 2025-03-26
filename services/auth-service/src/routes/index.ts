97528cd533e954befb6ea63f1ac88fb40f1e52bc
maxg56 <mgendrot56@gmail.com>
2025-03-26 21:56:43 +0100

    add | /auth/profile

--- services/auth-service/src/routes/index.ts
+++ services/auth-service/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify';
-import UsersController from '../controllers/users.controller';
 import AuthController from '../controllers/auth.controller';
 
 export default async function (fastify: FastifyInstance) {
   fastify.post('/auth/login',  AuthController.login);
   fastify.post('/auth/register', AuthController.register);
-  fastify.get('/auth/profile', { preHandler: [fastify.authenticate] },UsersController.getAllUsers);
+  fastify.get("/auth/profile", { preHandler: [fastify.authenticate] }, async (request: any, reply) => {
+    return request.user;
+  });
+  
+  
 }

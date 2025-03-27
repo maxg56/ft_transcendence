6fd216c1fed61a73a4bc0410cf520caeef07757f
maxg56 <mgendrot56@gmail.com>
2025-03-27 21:46:08 +0100

    add | api

--- frontend/src/pages/Accueil.tsx
+++ frontend/src/pages/Accueil.tsx
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
 import { X } from "lucide-react";
 import { useTranslation } from "../context/TranslationContext";
-import  useNavigation  from "../hooks/useNavigation";
+import useNavigation from "../hooks/useNavigation";
 import SettingsModal from "../components/SettingsModal";
+
 const Accueil: React.FC = () => {
-   
     const [isSignInOpen, setIsSignInOpen] = useState(false);
     const [isSignUpOpen, setIsSignUpOpen] = useState(false);
-    
-    //pour changer la langue
+
+    // Gestion de la langue
     const { t } = useTranslation();
 
-    //naviguation    
+    // Navigation
     const { navigate } = useNavigation();
 
-    // Variable pour stocker login et mdp   
+    // État des formulaires
     const [login, setLogin] = useState("");
     const [passwordSignIn, setPasswordSignIn] = useState("");
-
-    // Variable pour Sign Up
     const [username, setUsername] = useState("");
     const [mail, setMail] = useState("");
     const [passwordSignUp, setPasswordSignUp] = useState("");
     const [confirmPassword, setConfirmPass] = useState("");
 
-    // Fonction pour réinitialiser les valeurs des formulaires
+    // Réinitialisation des champs
     const resetModals = () => {
         setLogin("");
         setPasswordSignIn("");
@@ -32,82 +30,119 @@ const Accueil: React.FC = () => {
         setMail("");
         setPasswordSignUp("");
         setConfirmPass("");
-    }
+    };
 
-    // Fonction pour fermer les modales et réinitialiser les champs
+    // Fermeture des modales
     const closeModal = (modalType: string) => {
-        if (modalType === "signIn") {
-            setIsSignInOpen(false);
-        } else if (modalType === "signUp") {
-            setIsSignUpOpen(false);
-        } 
-        resetModals(); // Réinitialisation des champs à la fermeture de la modale
-    }
+        if (modalType === "signIn") setIsSignInOpen(false);
+        if (modalType === "signUp") setIsSignUpOpen(false);
+        resetModals();
+    };
+
+    // Connexion
+    const handleSignIn = async () => {
+        if (!login || !passwordSignIn) {
+            console.error("Veuillez remplir tous les champs.");
+            return;
+        }
+        console.log(`Connexion avec ${login}`);
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username :login,
+                password: passwordSignIn,
+            }),
+        };
+
+        try {
+            const response = await fetch("https://localhost:8443/auth/login", requestOptions);
+            if (!response.ok) throw new Error(`Erreur ${response.status}: ${await response.text()}`);
+
+            console.log("Inscription réussie");
+            closeModal("signUp");
+            navigate("/hub");
+        } catch (error) {
+            console.error("Erreur lors de l'inscription:", error);
+        }
+    };
+
+    // Inscription
+    const handleSignUp = async () => {
+        if (!username || !mail || !passwordSignUp || !confirmPassword) {
+            console.error("Tous les champs doivent être remplis");
+            return;
+        }
+        if (passwordSignUp !== confirmPassword) {
+            console.error("Les mots de passe ne correspondent pas");
+            return;
+        }
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                email: mail,
+                password: passwordSignUp,
+            }),
+        };
+
+        try {
+            const response = await fetch("https://localhost:8443/auth/register", requestOptions);
+            if (!response.ok) throw new Error(`Erreur ${response.status}: ${await response.text()}`);
+
+            console.log("Inscription réussie");
+            closeModal("signUp");
+            navigate("/hub");
+        } catch (error) {
+            console.error("Erreur lors de l'inscription:", error);
+        }
+    };
 
     return (
         <div>
-            {/* Header Titre avec Param bouton*/}
+            {/* Header */}
             <header className="bg-orange-300 p-4 text-white flex justify-center items-center relative">
                 FT_TRANSCENDENCE
-                <SettingsModal/>
+                <SettingsModal />
             </header>
 
-            {/* Body avec les boutons */}
+            {/* Boutons */}
             <div className="flex flex-col items-center gap-4 mt-4">
-                {/* Bouton Sign In */}
-                <button
-                    className="px-4 py-2 bg-orange-300 text-black rounded hover:bg-gray-200"
-                    onClick={() => setIsSignInOpen(true)} >
-                    {t ('Se connecter')}
+                <button className="px-4 py-2 bg-orange-300 text-black rounded hover:bg-gray-200"
+                    onClick={() => setIsSignInOpen(true)}>
+                    {t("Se connecter")}
                 </button>
 
-                {/* Bouton Sign Up */}
-                <button
-                    className="px-4 py-2 bg-orange-300 text-black rounded hover:bg-gray-200"
-                    onClick={() => setIsSignUpOpen(true)} >
-                    {t ('Inscription')}
+                <button className="px-4 py-2 bg-orange-300 text-black rounded hover:bg-gray-200"
+                    onClick={() => setIsSignUpOpen(true)}>
+                    {t("Inscription")}
                 </button>
-
             </div>
 
             {/* Modale Sign In */}
             {isSignInOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-                        onClick={() => closeModal("signIn")}>
+                    onClick={() => closeModal("signIn")}>
                     <div className="bg-white p-6 rounded-lg shadow-lg w-80 relative"
                         onClick={(e) => e.stopPropagation()}>
-                        {/* Croix pour fermer */}
-                        <button
-                            onClick={() => closeModal("signIn")}
-                            className="absolute top-2 right-2 text-gray-600 hover:text-red-500"
-                        >
+                        <button onClick={() => closeModal("signIn")} className="absolute top-2 right-2 text-gray-600 hover:text-red-500">
                             <X size={24} />
                         </button>
 
-                        {/* Formulaire */}
                         <h2 className="text-lg font-bold mb-4">Connexion</h2>
-                        <input
-                            type="text"
-                            placeholder={t("Nom d'utilisateur")}
-                            className="w-full px-3 py-2 border rounded mb-4"
-                            value={login}
-                            onChange={(e) => setLogin(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder={t("Mot de passe")}
-                            className="w-full px-3 py-2 border rounded mb-4"
-                            value={passwordSignIn}
-                            onChange={(e) => setPasswordSignIn(e.target.value)}
-                        />
-                        <button
-                            className={`px-4 py-2 rounded w-full ${login && passwordSignIn ? "bg-orange-300 text-black hover:bg-gray-200" : "bg-gray-300 text-gray-500 cursor-not-allowed"}`}
-                            onClick={() => {
-                                closeModal("signIn") // 🔥 Fermer la modale
-                                setTimeout(() => navigate("/hub"), 50)}} // 🔥 Petit delai pour laisser le temps à React de re-render
-                                disabled={!login || !passwordSignIn}
-                        >
-                            {t ('Se connecter')}
+                        <input type="text" placeholder={t("Nom d'utilisateur")} className="w-full px-3 py-2 border rounded mb-4"
+                            value={login} onChange={(e) => setLogin(e.target.value)} />
+                        <input type="password" placeholder={t("Mot de passe")} className="w-full px-3 py-2 border rounded mb-4"
+                            value={passwordSignIn} onChange={(e) => setPasswordSignIn(e.target.value)} />
+                        <button className={`px-4 py-2 rounded w-full ${login && passwordSignIn ? "bg-orange-300 text-black hover:bg-gray-200" : "bg-gray-300 text-gray-500 cursor-not-allowed"}`}
+                            onClick={handleSignIn} disabled={!login || !passwordSignIn}>
+                            {t("Se connecter")}
                         </button>
                     </div>
                 </div>
@@ -116,55 +151,25 @@ const Accueil: React.FC = () => {
             {/* Modale Sign Up */}
             {isSignUpOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-                        onClick={() => closeModal("signIn")}>
+                    onClick={() => closeModal("signUp")}>
                     <div className="bg-white p-6 rounded-lg shadow-lg w-80 relative"
-                            onClick={(e) => e.stopPropagation()}>
-                        {/* Croix pour fermer */}
-                        <button
-                            onClick={() => closeModal("signUp")}
-                            className="absolute top-2 right-2 text-gray-600 hover:text-red-500"
-                        >
+                        onClick={(e) => e.stopPropagation()}>
+                        <button onClick={() => closeModal("signUp")} className="absolute top-2 right-2 text-gray-600 hover:text-red-500">
                             <X size={24} />
                         </button>
 
-                        {/* Formulaire */}
-                        <h2 className="text-lg font-bold mb-4">{t('Inscription')}</h2>
-                        <input
-                            type="text"
-                            placeholder="Nom d'utilisateur"
-                            className="w-full px-3 py-2 border rounded mb-4"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            className="w-full px-3 py-2 border rounded mb-4"
-                            value={mail}
-                            onChange={(e) => setMail(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="Mot de passe"
-                            className="w-full px-3 py-2 border rounded mb-4"
-                            value={passwordSignUp}
-                            onChange={(e) => setPasswordSignUp(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="Confirmer le mot de passe"
-                            className="w-full px-3 py-2 border rounded mb-4"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPass(e.target.value)}
-                        />
-                        <button
-                            className={`px-4 py-2 rounded w-full ${username && mail && passwordSignUp && confirmPassword ? "bg-orange-300 text-black hover:bg-gray-200" : "bg-gray-300 text-gray-500 cursor-not-allowed"}`}
-                            onClick={() => {
-                                closeModal("signUp")
-                                setTimeout(() => navigate("/hub"), 50)}}
-                                disabled={!username || !mail || !passwordSignUp || !confirmPassword}
-                                >
-                            {t ('Inscription')}
+                        <h2 className="text-lg font-bold mb-4">{t("Inscription")}</h2>
+                        <input type="text" placeholder="Nom d'utilisateur" className="w-full px-3 py-2 border rounded mb-4"
+                            value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input type="email" placeholder="Email" className="w-full px-3 py-2 border rounded mb-4"
+                            value={mail} onChange={(e) => setMail(e.target.value)} />
+                        <input type="password" placeholder="Mot de passe" className="w-full px-3 py-2 border rounded mb-4"
+                            value={passwordSignUp} onChange={(e) => setPasswordSignUp(e.target.value)} />
+                        <input type="password" placeholder="Confirmer le mot de passe" className="w-full px-3 py-2 border rounded mb-4"
+                            value={confirmPassword} onChange={(e) => setConfirmPass(e.target.value)} />
+                        <button className={`px-4 py-2 rounded w-full ${username && mail && passwordSignUp && confirmPassword ? "bg-orange-300 text-black hover:bg-gray-200" : "bg-gray-300 text-gray-500 cursor-not-allowed"}`}
+                            onClick={handleSignUp} disabled={!username || !mail || !passwordSignUp || !confirmPassword}>
+                            {t("Inscription")}
                         </button>
                     </div>
                 </div>

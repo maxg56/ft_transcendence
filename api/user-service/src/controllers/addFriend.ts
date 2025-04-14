e6d27a07c422e1dfcf6b6651d386b3bfe4789b15
ManonPe <73585054+ManonPe@users.noreply.github.com>
2025-04-14 18:17:07 +0200

    feat: add friend

--- api/user-service/src/controllers/addFriend.ts
+++ api/user-service/src/controllers/addFriend.ts
@@ -1,20 +1,22 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { Op } from 'sequelize';
-import User from '../models/User'
 import Friendship from '../models/Friendship';
 
 export const addFriend = async (request: FastifyRequest, reply: FastifyReply) => {
 	try {
-		const {user1, user2} = request.body as {user:  }
+		const {user1, user2} = request.body as {user1: number, user2: number}
 		const isFriend = await Friendship.findOne({
 			where: {
 				[Op.or]: [
-				{ user1: player.id, user2: host.id },
-				{ user1: host.id, user2: player.id },
-				],
-			},
-		  })
-		
+				{ user1, user2 },
+				{ user2, user1 }],
+			}
+		})
+
+		if (isFriend)
+			return reply.code(409).send({ message: 'Request already send'});
+		const friendship = await Friendship.create({ user1, user2, status: 'pending'})
+		return reply.send({message: "Friend request send"});
 	} catch (error) {
 		request.log.error(error);
 		return reply.code(500).send({message: 'server error'});

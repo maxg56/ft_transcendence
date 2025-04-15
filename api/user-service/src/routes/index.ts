4febe499812f736d1e3ea59ad319c7e69d32c2fd
ManonPe <73585054+ManonPe@users.noreply.github.com>
2025-04-15 11:04:37 +0200

    feat: get all users by their username

--- api/user-service/src/routes/index.ts
+++ api/user-service/src/routes/index.ts
@@ -2,12 +2,14 @@ import fp from 'fastify-plugin'
 import {putUser, getUser} from '../controllers/userControllers'
 import { getRank } from '../controllers/getRank';
 import { addFriend } from '../controllers/addFriend';
+import { getAllUsernames } from '../controllers/getAllUsernames';
 
 
 async function userRoutes(fastify: any) {
   fastify.put('/user/:id', putUser);
   fastify.get('/user/:id', getUser);
   fastify.get('/user/:id/rank', getRank);
+  fastify.get('/user/users', getAllUsernames);
   fastify.post('/user/:id/friend', addFriend);
 }
 

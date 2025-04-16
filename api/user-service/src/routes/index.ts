312d3cef7179ba0f246424c28d0183dc90aa4171
ManonPe <73585054+ManonPe@users.noreply.github.com>
2025-04-16 16:14:23 +0200

    fix(friend): add a token for addFriend

--- api/user-service/src/routes/index.ts
+++ api/user-service/src/routes/index.ts
@@ -9,7 +9,7 @@ async function userRoutes(fastify: any) {
   fastify.get('/user/:id', getUser);
   fastify.get('/user/:id/rank', getRank);
   fastify.get('/user/users', getAllUsernames);
-  fastify.post('/user/:id/friend', addFriend);
+  fastify.post('/user/friend/add', { preHandler: [fastify.authenticate] }, addFriend);
   fastify.put('/user/friend/accept', { preHandler: [fastify.authenticate] }, acceptFriend);
   fastify.get('/user/friend/pendinglist', { preHandler: [fastify.authenticate] }, seeFriendRequests);
   fastify.put('/user/friend/refuse', { preHandler: [fastify.authenticate] }, refuseFriend);

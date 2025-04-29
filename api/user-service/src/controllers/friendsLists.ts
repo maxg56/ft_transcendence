d9cd15b0a57fabb9d827a8bf04f5482324cc5830
ThomasL6 <t.lefebvre53@gmail.com>
2025-04-29 18:34:58 +0200

    fix(user) : modification pending list

--- api/user-service/src/controllers/friendsLists.ts
+++ api/user-service/src/controllers/friendsLists.ts
@@ -5,13 +5,12 @@ import User from '../models/User';
 import { sendError, sendSuccess } from '../utils/reply';
 
 async function seeFriendRequests (request: FastifyRequest, reply: FastifyReply) {
+	try{
 		const id = request.user.id
 		console.log("🧩 user ID:", id)
 		const friends= await Friendship.findAll({ where: { 
-				[Op.or]: [
-					{ user1: id },
-					{ user2: id }],
-				status: 'pending'
+			user2: id,
+			status: 'pending'
 			},
 			include: [{
 				model: User,
@@ -33,11 +32,51 @@ async function seeFriendRequests (request: FastifyRequest, reply: FastifyReply) 
 				avatar: otherUser.avatar
 			};
 		})
-		return pendingList
+		return sendSuccess(reply, pendingList, 200)
+	} catch (error) {
+		request.log.error(error);
+		return sendError(reply, 'server error', 500)
+	}
 }
 
 async function seeFriends (request: FastifyRequest, reply: FastifyReply) {
 	const id = request.user.id
+		console.log("🧩 user ID:", id)
+		const friends = await Friendship.findAll({ where: { 
+				[Op.or]: [
+					{ user1: id },
+					{ user2: id }],
+				status: 'accepted'
+			},
+			include: [{
+				model: User,
+				as: 'userOne',
+				attributes: ['username', 'avatar'],
+			},
+			{	model: User,
+				as: 'userTwo',
+				attributes: ['username', 'avatar'],
+			}]});
+		
+		// if (friends.length === 0)
+		// 	return sendError(reply, 'No friend in the list', 404)
+
+		const friendList = friends.map(friendship => {
+			const isUser1 = friendship.user1 === id;
+			const otherUser = isUser1 ? friendship.userTwo : friendship.userOne;
+			return {
+				username: otherUser.username,
+				avatar: otherUser.avatar
+			};
+		})
+		return friendList
+}
+
+async function getFriendsList (request: FastifyRequest, reply: FastifyReply) {
+	try {
+		// const acceptedList = seeFriends(request, reply)
+		// const pendingList = seeFriendRequests(request, reply)
+		const id = request.user.id
 		console.log("🧩 user ID:", id)
 		const friends = await Friendship.findAll({ where: { 
 				[Op.or]: [
@@ -66,19 +105,36 @@ async function seeFriends (request: FastifyRequest, reply: FastifyReply) {
 				avatar: otherUser.avatar
 			};
 		})
-		return friendList
-}
 
-async function getFriendsList (request: FastifyRequest, reply: FastifyReply) {
-	try {
-		const acceptedList = seeFriends(request, reply)
-		const pendingList = seeFriendRequests(request, reply)
+		const pending= await Friendship.findAll({ where: { 
+			user1: id,
+			status: 'pending'
+			},
+			include: [{
+				model: User,
+				as: 'userOne',
+				attributes: ['username', 'avatar'],
+			},
+			{	model: User,
+				as: 'userTwo',
+				attributes: ['username', 'avatar'],
+			}]});
 		
-		return sendSuccess(reply, {acceptedList, pendingList}, 200)
+		// if (pending.length === 0)
+		// 	return sendError(reply, 'No friend request pending', 404)
+		const pendingList = pending.map(friendship => {
+			const isUser1 = friendship.user1 === id;
+			const otherUser = isUser1 ? friendship.userTwo : friendship.userOne;
+			return {
+				username: otherUser.username,
+				avatar: otherUser.avatar
+			};
+		})
+		return sendSuccess(reply, {friendList, pendingList}, 200)
 	} catch (error) {
 		request.log.error(error);
 		return sendError(reply, 'server error', 500)
 	}
 }
 
-export {getFriendsList}
+export {getFriendsList, seeFriendRequests}

b1f65b87b1b737a3a7f51ad01ed3bf05125ba977
maxg56 <mgendrot56@gmail.com>
2025-04-30 17:39:11 +0200

    feat(database): implement retry logic for database connection attempts

--- api/stats-service/src/plugins/database.ts
+++ api/stats-service/src/plugins/database.ts
@@ -1,18 +1,24 @@
 import fp from 'fastify-plugin';
+import { Sequelize } from 'sequelize';
 import sequelize from '../config/database';
 
 async function databasePlugin(fastify: any) {
-  try {
-    await sequelize.authenticate();
-    console.log('✅ Database connected successfully');
+  const maxRetries = 5;
+  const retryDelay = 5000; // 2 seconds
 
-    // await sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
-		// await sequelize.sync({ alter: true })
-		// await sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
-		// console.log('✅ sync force/alter all tables')
-  } catch (error) {
-    console.error('❌ Unable to connect to the database:', error);
-    throw error;
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      await sequelize.authenticate();
+      console.log('✅ Database connected successfully');
+      break;
+    } catch (error) {
+      console.error(`❌ Unable to connect to the database (Attempt ${attempt}/${maxRetries}):`, error);
+      if (attempt === maxRetries) {
+        throw error;
+      }
+      console.log(`Retrying in ${retryDelay / 1000} seconds...`);
+      await new Promise((resolve) => setTimeout(resolve, retryDelay));
+    }
   }
 
   fastify.decorate('db', sequelize);
--- api/user-service/src/plugins/database.ts
+++ api/user-service/src/plugins/database.ts
@@ -32,29 +32,40 @@ async function addColumnIfNotExists(tableName: string, newColumn: string, data: 
 // console.log('sync force all tables')
 // desactive les contraintes de cles etrangeres entre table, drop les tables,
 // create les tables et reactive les contraintes de cles etrangeres
+// addColumnIfNotExists('user', 'lastLogin_at', {
+		
+
 async function databasePlugin(fastify: any) {
-	try {
+	const maxRetries = 5;
+	const retryDelay = 5000; // 2 seconds
+  
+	for (let attempt = 1; attempt <= maxRetries; attempt++) {
+	  try {
 		await sequelize.authenticate();
 		console.log('✅ Database connected successfully');
-		// addColumnIfNotExists('user', 'lastLogin_at', {
 		// 	type: DataTypes.DATE,
 		// 	allowNull: false,
 		// 	defaultValue: DataTypes.NOW,
 		// })
 		// await User.sync({ alter: true })
 		// console.log("✅ User table synchronized")
-		
+			
 		// await sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
 		// await sequelize.sync({ alter: true })
 		// await sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
 		// console.log('sync force/alter all tables')
-		
-	} catch (error) {
-		console.error('❌ Unable to connect to the database:', error);
-		throw error;
+		break;
+	  } catch (error) {
+		console.error(`❌ Unable to connect to the database (Attempt ${attempt}/${maxRetries}):`, error);
+		if (attempt === maxRetries) {
+		  throw error;
+		}
+		console.log(`Retrying in ${retryDelay / 1000} seconds...`);
+		await new Promise((resolve) => setTimeout(resolve, retryDelay));
+	  }
 	}
-
+  
 	fastify.decorate('db', sequelize);
-}
+  }
 
 export default fp(databasePlugin);

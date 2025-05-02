700b6df8de2f159d5fb8413b957fb068dfeaaedd
ManonPe <73585054+ManonPe@users.noreply.github.com>
2025-05-02 17:05:23 +0200

    fix(user): all usernames for add friend gives not friend only

--- api/user-service/src/controllers/getAllUsernames.ts
+++ api/user-service/src/controllers/getAllUsernames.ts
@@ -2,18 +2,45 @@ import { FastifyRequest, FastifyReply } from 'fastify';
 import User from '../models/User'
 import { sendSuccess, sendError } from '../utils/reply';
 import { Op } from 'sequelize';
+import Friendship from '../models/Friendship';
 
 export const getAllUsernames = async (request: FastifyRequest, reply:FastifyReply) => {
 	try {
 		const id = request.user.id
+
+		const friendships = await Friendship.findAll({
+			where: {
+				[Op.and]: [{
+					[Op.or]: [
+						{ status: 'accepted' },
+						{ status: 'pending' }
+					]},
+					{[Op.or]: [
+						{ user1: id },
+						{ user2: id }
+					]}
+				]
+			},
+			attributes: ['user1', 'user2']
+		})
+
+		const friendIds = new Set<number>()
+		for (const f of friendships) {
+			if (f.user1 !== id) friendIds.add(f.user1)
+			if (f.user2 !== id) friendIds.add(f.user2)
+		}
+
 		const users = await User.findAll({
 			where: {
-				[Op.not]: [
-					{ username: {
-					[Op.substring]: 'deleted'
-					}}
-				]},
-			attributes: ['username']});
+				id: {
+					[Op.notIn]: [id, ...friendIds]
+				},
+				username: {
+					[Op.notLike]: 'deleted%',
+				}
+			},
+			attributes: ['username']
+		})
 
 		if (!users)
 			return reply.code(404).send({ message: 'user not find'});

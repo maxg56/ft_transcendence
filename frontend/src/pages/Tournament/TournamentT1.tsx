797ed38fcfaea542707a42fdac3c320f6ad0bbde
maxg56 <mgendrot56@gmail.com>
2025-05-04 22:24:19 +0200

    refactor(profile, tournament, game): streamline success handlers and remove unused imports

--- frontend/src/pages/Tournament/TournamentT1.tsx
+++ frontend/src/pages/Tournament/TournamentT1.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import useNavigation from "@/hooks/useNavigation";
 import { useTournament } from '@/context/ResultsContext';
 

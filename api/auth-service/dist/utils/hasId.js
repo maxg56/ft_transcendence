2affe3379b61a971a2fd68b6bd894cfb5903b7bc
maxg56 <mgendrot56@gmail.com>
2025-05-04 23:10:44 +0200

    refactor(auth): remove unused authentication utilities and related files

--- api/auth-service/dist/utils/hasId.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hasId = hasId;
-function hasId(value) {
-    return typeof value === 'object' && value !== null && 'id' in value;
-}

364a4dcdde38a64d5bea15b21bd68d33cc290002
ThomasL6 <t.lefebvre53@gmail.com>
2025-05-07 18:39:38 +0200

    (feat(chat) ajout notif en cas de mail)

--- frontend/src/components/chat/ConversationPanel.tsx
+++ frontend/src/components/chat/ConversationPanel.tsx
@@ -1,35 +1,47 @@
-import React, {useEffect, useRef} from "react";
+import React from "react";
 
 interface Props {
 	messages: string[];
 	selectedUser: string | null;
 }
 
+// const currentUser = "Moi"; // Nom fictif pour toi
+
 const ConversationPanel: React.FC<Props> = ({ messages, selectedUser }) => {
-	
-	const endRef = useRef<HTMLDivElement | null>(null);
-	
-	useEffect(() => {
-		if(endRef.current)
-			endRef.current.scrollIntoView({behavior: "smooth"})
-	})
+	if (!selectedUser) {
+		return (
+			<div className="flex items-center justify-center h-full text-gray-500">
+				Sélectionne un utilisateur pour commencer une conversation
+			</div>
+		);
+	}
 
 	return (
-		<div className="flex flex-col overflow-y-auto justify-end space-y-3 bg-gray-100 rounded-2xl  scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-blue-200">
-			{selectedUser && (
-				<div className="text-lg text-center font-semibold text-gray-700 ">
-					{selectedUser}
-				</div>
-			)}
-			{messages.map((msg, idx) => (
-				<div
-					key={idx}
-					className="bg-blue-100 max-w-xs self-end rounded-lg px-3 py-2 text-sm break-words "
-				>
-					{msg}
-				</div>
-			))}
-			<div ref={endRef} />
+		<div className="space-y-2 bg-gray-200 rounded-2xl">
+			{messages.map((msg, index) => {
+				const isFromMe = !msg.startsWith(`${selectedUser}: `);
+
+				const content = isFromMe ? msg : msg.replace(`${selectedUser}: `, "");
+
+				return (
+					<div
+						key={index}
+						className={`flex ${
+							isFromMe ? "justify-end" : "justify-start"
+						}`}
+					>
+						<div
+							className={`px-4 py-2 rounded-lg max-w-xs text-sm ${
+								isFromMe
+									? "bg-blue-500 text-white"
+									: "bg-gray-300 text-black"
+							}`}
+						>
+							{content}
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 };

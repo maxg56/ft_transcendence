e482b959a1f0ce7dc0373066913f0e8242278ec7
ManonPe <73585054+ManonPe@users.noreply.github.com>
2025-05-08 10:50:09 +0200

    fix(frontprofil): same elo in header and in graph

--- api/user-service/src/controllers/getRank.ts
+++ api/user-service/src/controllers/getRank.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import User from '../models/User'
+import MatchPlayer from '../models/MatchPlayer';
 import { sendSuccess, sendError } from '../utils/reply';
 import { hasId } from '../utils/hasId';
 
@@ -14,13 +15,17 @@ try {
 	if (typeof id !== 'number' || isNaN(id)) {
 	  return sendError(reply, 'Invalid user ID', 400);
 	}
-	const elo = await User.findByPk(id, {attributes: ['elo']});
-
-	if (!elo)
-		return reply.code(404).send({ message: 'elo not find' });
-
-	return sendSuccess(reply, elo, 200);
-
+	const elo_gain = await MatchPlayer.findAll({
+		where: { player_id: id },
+		attributes: ['elo_change'],
+		order:  [['match_id', 'ASC']],
+	})
+	let elo_now = 1000;
+	const elos = elo_gain.map(match => {
+		elo_now += match.elo_change;
+		return { elo: elo_now }
+	})
+	return sendSuccess(reply, elos[elos.length - 1], 200)
 } catch (error) {
 	request.log.error(error);
 	return sendError(reply, 'server error', 500);

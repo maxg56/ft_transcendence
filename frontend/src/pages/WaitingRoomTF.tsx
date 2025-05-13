b555adc69013d171c6652ca39d2483b77cd11554
ManonPe <73585054+ManonPe@users.noreply.github.com>
2025-05-13 16:21:26 +0200

    feat(design): base of style on tournament

--- frontend/src/pages/WaitingRoomTF.tsx
+++ frontend/src/pages/WaitingRoomTF.tsx
@@ -8,8 +8,13 @@ const WaitingRoomTF = () => {
   const { code, players ,isTournament} = useWaitroomListener();
 
   return (
-    <div className="min-h-screen bg-gray-50 p-8 flex flex-col items-center justify-center">
-      <div className="text-center mb-8">
+    <div className="scale-95">
+    <div className="crt w-screen h-screen rounded-[150px] padding-[10px] overflow-hidden bg-gray-900 flex flex-col">
+      {/* CRT Scanline Sweep */}
+      <div className="absolute inset-0 z-10 pointer-events-none overflow-hidden">
+            <div className="w-full h-full absolute top-[-100%] scanline-glow" />
+          </div>
+      <div className="text-center mb-8 text-white">
         <h2 className="text-2xl font-bold mb-2">Salle d'attente Tournoi</h2>
         <div className="mb-4">
           <span className="font-semibold">Code du tournoi :</span>
@@ -20,6 +25,7 @@ const WaitingRoomTF = () => {
         </div>
         <ParticipantsList players={players} code={code} isTournament={isTournament} />
       </div>
+    </div>
     </div>
   );
 };

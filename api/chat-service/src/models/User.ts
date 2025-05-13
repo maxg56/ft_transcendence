46a2a6ac1bdfccae7e9fdc83b3b9ee10d1617a92
ManonPe <73585054+ManonPe@users.noreply.github.com>
2025-05-13 18:58:10 +0200

    fix(DB): user in chat up to date with DB limit message

--- api/chat-service/src/models/User.ts
+++ api/chat-service/src/models/User.ts
@@ -12,6 +12,8 @@ class User extends Model {
   public created_at!: Date;
   public updated_at!: Date;
   public lastLogin_at!: Date;
+  public twoFactorSecret!: string | null;
+  public is2FAEnabled!: boolean;
 }
 
 User.init(
@@ -65,7 +67,15 @@ User.init(
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
-    }
+    },
+    twoFactorSecret: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    is2FAEnabled: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
   {
     sequelize,
